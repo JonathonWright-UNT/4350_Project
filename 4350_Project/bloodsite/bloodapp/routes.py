91a566fa322dc570350ce53c663eede01804a228
Jonathon Wright <jonathonwright@my.unt.edu>
2020-11-29 13:52:06 -0800

    new db

--- 4350_Project/bloodsite/bloodapp/routes.py
+++ 4350_Project/bloodsite/bloodapp/routes.py
@@ -1,5 +1,4 @@
 import datetime
-
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 from bloodapp.forms import CreateDonorForm, UpdateDonorForm, DonorForm, DonationForm, WithdrawForm, CreateEmployeeForm, LoginForm
